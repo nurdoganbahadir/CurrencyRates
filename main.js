5e2e0c562efd9427e1de546956333882be883964
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-07 12:21:18 +0300

    event function added

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ const amount = document.querySelector("#amount");
 const firstOption = document.querySelector("#firstCurrencyOption");
 const secondOption = document.querySelector("#secondCurrencyOption");
 const resultInput = document.querySelector("#result");
+
+runEventListeners();
+
+function runEventListeners() {
+  amount.addEventListener("input", exchange);
+}
+
+function exchange() {
+  
+}
