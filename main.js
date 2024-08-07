8a84caaa8cae66f652923d7feb5fb2f05e8e743d
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-07 12:43:52 +0300

    exchange function update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const amount = document.querySelector("#amount");
+const amountInput = document.querySelector("#amount");
 const firstOption = document.querySelector("#firstCurrencyOption");
 const secondOption = document.querySelector("#secondCurrencyOption");
 const resultInput = document.querySelector("#result");
@@ -6,9 +6,15 @@ const resultInput = document.querySelector("#result");
 runEventListeners();
 
 function runEventListeners() {
-  amount.addEventListener("input", exchange);
+  amountInput.addEventListener("input", exchange);
 }
 
 function exchange() {
-  
+  const amount = Number(amountInput.value.trim());
+  const firstOptionValue =
+    firstOption.options[firstOption.selectedIndex].textContent;
+
+  const secondOptionValue =
+    secondOption.options[secondOption.selectedIndex].textContent;
+  console.log(firstOptionValue, secondOptionValue);
 }
