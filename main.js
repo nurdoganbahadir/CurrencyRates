f58f24d2270e05dab01d28837e4dc017af65c562
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-07 12:19:08 +0300

    js selector added

---
+++ main.js
@@ -0,0 +1,4 @@
+const amount = document.querySelector("#amount");
+const firstOption = document.querySelector("#firstCurrencyOption");
+const secondOption = document.querySelector("#secondCurrencyOption");
+const resultInput = document.querySelector("#result");
